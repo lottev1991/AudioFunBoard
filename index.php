bd5e67e515a447a171ce64414d76fbc1c14c924f
Lotte V <61566058+lottev1991@users.noreply.github.com>
2026-02-02 02:06:56 +0100

    a

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             });
         }
     </script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
 </head>
 
 <body>
